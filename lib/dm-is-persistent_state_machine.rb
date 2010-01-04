86bb753ebe9f56c7ff970b4bc8e86cf920d9ceb7
michael <ma@zive.at>
2010-01-04 18:37:04 +0100

    fixed gem layout

--- lib/dm-is-persistent_state_machine.rb
+++ lib/dm-is-persistent_state_machine.rb
@@ -7,10 +7,6 @@ require 'dm-timestamps'
 require 'dm-validations'
 require 'dm-aggregates'
 
-
-# Temporary workaround until this is extracted into a gem
-$LOAD_PATH.unshift(File.expand_path('../../lib', __FILE__))
-
 require 'dm-is-persistent_state_machine/is/persistent_state_machine'
 
 # Activate the plugin
